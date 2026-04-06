b9b16cf26c359094aead82d49dccdf7fb274b559
Victor <el.rachman@unesp.br>
2026-04-06 15:42:34 -0300

    responsividade10

--- css-js/efeitos.js
+++ css-js/efeitos.js
@@ -1,4 +1,4 @@
-let texto1 = "A alimentação saudável é essencial para o funcionamento do nosso corpo, pois é dela que retiramos os nutrientes de que necessitamos para nossa sobrevivência. Uma alimentação saudável está relacionada com diversos benefícios para nosso organismo, como a melhora na imunidade, no humor e na qualidade do sono, o aumento da disposição, a regulação do nosso intestino e a manutenção do peso.";
+let texto1 = "A alimentação saudável é essencial para o funcionamento do nosso corpo, pois é dela que retiramos os nutrientes de que necessitamos para nossa sobrevivência. \n\n  Uma alimentação saudável está relacionada com diversos benefícios para nosso organismo, como a melhora na imunidade, no humor e na qualidade do sono, o aumento da disposição, a regulação do nosso intestino e a manutenção do peso.";
 let i = 0;
 let p = document.getElementById("texto1");
 
